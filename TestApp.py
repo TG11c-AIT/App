7470abad8eaa25074798f6a301a1a8b1c7c978de
Simeon Mesgol <simeon.mesgol@gmx.de>
2018-05-10 10:24:13 +0200

    Beispiel

--- TestApp.py
+++ TestApp.py
@@ -1,2 +1,14 @@
 # todo: Make a Application that is good.
 # todo: be a god.
+
+# Beispiel einer Methode.
+def test(wert1, wert2):
+    if(wert1==wert2):
+        true = "true"
+        return true
+
+wert1 = input("Please enter a digit: ")
+wert2 = input("Please enter a digit: ")
+
+x = test(wert1, wert2)
+print(x)
